6be2be05f7a13af60afdffd37ff36162bd387900
Dan Blakeman <githubdan@gmail.com>
2018-01-27 15:53:19 +0000

    Add temperature average to graphs

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,20 +1,24 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import {Sparklines, SparklinesLine } from 'react-sparklines';
+
+import Chart from '../components/chart'
+import GoogleMap from '../components/google_map'
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
+
     const temps = cityData.list.map( weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const {lon, lat} = cityData.city.coord;
 
     return (
         <tr key={name}>
-          <td>{name}</td>
-          <td>
-            <Sparklines height={120} width={180} data={temps}>
-              <SparklinesLine color="red" />
-            </Sparklines>
-          </td>
+          <td><GoogleMap lon={lon} lat={lat}></GoogleMap></td>
+          <td><Chart data={temps} color="orange" units="K"/></td>
+          <td><Chart data={pressures} color="green" units="hPa"/></td>
+          <td><Chart data={humidities} color="black" units="%"/></td>
         </tr>
     )
   }
